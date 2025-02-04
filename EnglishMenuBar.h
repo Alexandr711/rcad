32f14969b1b5d27f6a1c27596ba35390f2d8b203
Alexandr711 <65649872+Alexandr711@users.noreply.github.com>
2025-02-04 16:19:10 +0300

    Create EnglishMenuBar.h

---
+++ EnglishMenuBar.h
@@ -0,0 +1,14 @@
+#include <QString>
+
+\\Текст меню "Файл"
+const QString fileMenuNameStr = "File";
+const QString createFileNameStr = "Create file";
+const QString openFileNameStr = "Open file";
+const QString createProjectNameStr = "Create project";
+const QString openProjectNameStr = "Open project";
+
+const QString quitNameStr = "Quit";
+
+\\Текст меню "Правка"
+const QString editMenuNameStr = "Edit";
+const
