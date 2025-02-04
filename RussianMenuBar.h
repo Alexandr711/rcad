6d6db5845623ea7d0e33c51299f37eb7b9ee4c34
Alexandr711 <65649872+Alexandr711@users.noreply.github.com>
2025-02-04 16:14:44 +0300

    Create RussianMenuBar.h

---
+++ RussianMenuBar.h
@@ -0,0 +1,14 @@
+#include <QString>
+
+\\Текст меню "Файл"
+const QString fileMenuNameStr = "Файл";
+const QString createFileNameStr = "Создать файл";
+const QString openFileNameStr = "Открыть файл";
+const QString createProjectNameStr = "Создать проект";
+const QString openProjectNameStr = "Открыть проект";
+
+const QString quitNameStr = "Выход";
+
+\\Текст меню "Правка"
+const QString editMenuNameStr = "Правка";
+const
